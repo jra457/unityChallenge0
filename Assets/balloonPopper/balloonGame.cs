2c61f22c6e8a7c9b7c26520dc2789cdd22d28f2b
Jacob <jra457@msstate.edu>
2024-09-02 17:04:54 -0500

    Implemented scaling and mouse clicks

--- Assets/balloonPopper/balloonGame.cs
+++ Assets/balloonPopper/balloonGame.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class balloonGame : MonoBehaviour
 {
+    public int scoreToGive = 1;
+    public int clicksToPop = 5;
+    public float scaleIncreasePerClick = 0.1f;
+
     void OnMouseDown ()
     {
-        
+        clicksToPop -= 1;
+
+        transform.localScale += Vector3.one * scaleIncreasePerClick;
     }
 }
