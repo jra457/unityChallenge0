5a9980b91469e2011146750de9e576dd9479a1ab
Jacob <jra457@msstate.edu>
2024-09-02 18:08:32 -0500

    Added destroy mechanic

--- Assets/balloonPopper/balloonGame.cs
+++ Assets/balloonPopper/balloonGame.cs
@@ -7,11 +7,18 @@ public class balloonGame : MonoBehaviour
     public int scoreToGive = 1;
     public int clicksToPop = 5;
     public float scaleIncreasePerClick = 0.1f;
+    public ScoreManager scoreManager; 
 
     void OnMouseDown ()
     {
         clicksToPop -= 1;
 
         transform.localScale += Vector3.one * scaleIncreasePerClick;
+
+        if(clicksToPop == 0)
+        {
+            scoreManager.increaseScore(scoreToGive);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/balloonPopper/scoreManager.cs
+++ Assets/balloonPopper/scoreManager.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scoreManager : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int score;
 
-    // Update is called once per frame
-    void Update()
+    public void increaseScore (int amount)
     {
-        
+        score += amount;
     }
 }
