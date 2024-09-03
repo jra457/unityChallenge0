40f7e338e27fe41ff5a01aea6ff407c342cbae9c
Jacob <jra457@msstate.edu>
2024-09-02 20:01:30 -0500

    Setup update score function

--- Assets/balloonPopper/scoreManager.cs
+++ Assets/balloonPopper/scoreManager.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score;
+    public TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        updateScoreText();
+    }
 
     public void increaseScore (int amount)
     {
         score += amount;
+        updateScoreText();
+    }
+
+    void updateScoreText()
+    {
+        scoreText.text = "Score: " + score;
     }
 }
